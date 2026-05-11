9ea4b6fa2fb51e2800d3f9c40b6d1c4adc57dfda
Nekonic <97237986+Nekonic@users.noreply.github.com>
2026-05-11 10:03:22 +0900

    fix: 32bit

--- APS/해킹시연/toolchain-mingw.cmake
+++ APS/해킹시연/toolchain-mingw.cmake
@@ -1,14 +1,14 @@
-# Windows x86_64 크로스 컴파일 툴체인 (Linux → Windows)
-# 사용: cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw.cmake
+# Windows x86 (32-bit) 크로스 컴파일 툴체인 (Linux → Windows)
+# 사용: cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw32.cmake
 
 set(CMAKE_SYSTEM_NAME    Windows)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_SYSTEM_PROCESSOR x86)
 
-set(CMAKE_C_COMPILER    x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER  x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER   x86_64-w64-mingw32-windres)
+set(CMAKE_C_COMPILER    i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER  i686-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER   i686-w64-mingw32-windres)
 
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
