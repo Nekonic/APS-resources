c9dcf634985d0ebcd68eb96e5983699273622fe9
Nekonic <97237986+Nekonic@users.noreply.github.com>
2026-05-11 10:06:06 +0900

    Update build.sh

--- APS/해킹시연/build.sh
+++ APS/해킹시연/build.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "=== [1/3] 크로스 컴파일 도구 설치 ==="
 apt-get update -qq
-apt-get install -y cmake build-essential mingw-w64 wget
+apt-get install -y cmake build-essential mingw-w64 wget gcc-mingw-w64-i686 g++-mingw-w64-i686
 
 echo ""
 echo "=== [2/3] CMake 설정 ==="
